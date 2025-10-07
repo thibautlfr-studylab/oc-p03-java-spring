dfd9551404d8e9f214adabcaa4ec2a1f03d53ffa
Thibaut LEFRANCOIS <thibaut.lfr@gmail.com>
2025-10-07 11:20:57 +0200

    feat: initialize backend API project with Spring initializr setup and basic structure

---
+++ api/src/test/java/com/openclassrooms/chatop/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.chatop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
